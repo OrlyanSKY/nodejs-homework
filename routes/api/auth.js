68aaccadff750ea1d52821088995fcf0f0cd12d0
orlyanSKY <000slim001@gmail.com>
2023-01-31 10:20:36 +0300

    token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,5 +6,6 @@ const { auth: ctrl } = require("../../controllers");
 const { joiSchema } = require("../../models/user");
 
 router.post("/signup", validation(joiSchema), controllerWrap(ctrl.signup));
-router.get("/login");
+router.get("/login", validation(joiSchema), controllerWrap(ctrl.login));
+
 module.exports = router;
